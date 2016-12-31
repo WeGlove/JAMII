e0e1026bdbf1f2621af024551efa6acc337e1897
Faeolin9 <johannesihl@yahoo.de>
2016-12-31 15:10:06 +0100

    Added Wall tests
    made Donald Trump great again

--- src/board/wall/WallImplementation.java
+++ src/board/wall/WallImplementation.java
@@ -4,11 +4,24 @@ public class WallImplementation implements Wall {
 int x,y, ex, ey;
 
 public WallImplementation (int xX, int yY,int  exX,int eyY)
-{
+{ if (xX >= 0)
 	x = xX;
+else
+{
+	x = ~xX;
+}
+if (yY >= 0)
 	y = yY;
+else
+	y = ~yY;
+if (exX >= 0)
 	ex = exX;
+else
+	ex = ~exX;
+if (eyY >= 0)
 	ey = eyY;
+else 
+	ey = ~eyY;
 }
 	@Override
 	public int getStartX() {
