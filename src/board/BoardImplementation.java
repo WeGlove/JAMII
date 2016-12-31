28c73fa20b546d8e7d6bb9b567658e7ada1b4754
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2016-12-31 15:05:09 +0100

    Added BoardImplementation and tests for it

---
+++ src/board/BoardImplementation.java
@@ -0,0 +1,57 @@
+package board;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import board.light.Light;
+import board.wall.Wall;
+import image.Image;
+
+public class BoardImplementation implements Board {
+
+	int width, height;
+	List <Light> LightList;
+	List <Wall> WallList;
+	
+	public BoardImplementation(int width, int height){
+		if (width<1 || height < 1) {
+			width=1;
+			height=1;
+		}
+		else{
+			this.width  = width;
+			this.height = height;	
+		}
+		LightList = new ArrayList<Light>();
+		WallList = new ArrayList<Wall>();
+	}
+	
+	@Override
+	public int getWidth() {
+		return width;
+	}
+
+	@Override
+	public int getHeight() {
+		return height;
+	}
+
+	@Override
+	public List<Light> getLightList() {
+		return LightList;
+	}
+
+	@Override
+	public List<Wall> getWallList() {
+		return WallList;
+	}
+
+	@Override
+	public Image computeImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 import org.junit.Test;
 
 public class Main {
-@Test
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //Penis
