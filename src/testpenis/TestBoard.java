305cff826b49cf4c3ed7338d001082f606ccf796
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2016-12-31 15:45:46 +0100

    Implemented test
    Tests are currently all working

--- src/testpenis/TestBoard.java
+++ src/testpenis/TestBoard.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import board.Board;
 import board.BoardImplementation;
+import board.light.Lightimplementation;
 
 public class TestBoard {
 
@@ -13,4 +14,44 @@ public class TestBoard {
 		assert(b.getWidth()==2);
 	}
 	
+	@Test
+	public void BoardTestHeight(){
+		Board b = new BoardImplementation(2,2);
+		assert(b.getHeight()==2);
+	}
+
+	@Test
+	public void BoardTestHeightWidth(){
+		Board b = new BoardImplementation(2,2);
+		assert(b.getHeight()==2 && b.getWidth()==2);
+	}
+	
+	@Test
+	public void BoardTestWidthTooSmall(){
+		Board b = new BoardImplementation(-2,2);
+		assert(b.getWidth()==1);
+	}
+	
+	@Test
+	public void BoardTestHeightTooSmall(){
+		Board b = new BoardImplementation(2,-2);
+		assert(b.getHeight()==1);
+	}
+
+	@Test
+	public void BoardTestWidthHeightTooSmall(){
+		Board b = new BoardImplementation(-2,-2);
+		assert(b.getHeight()==1 && b.getWidth()==1);
+	}
+	
+	public void BoardTestLightListNotNull(){
+		Board b = new BoardImplementation(2,2);
+		assert(b.getLightList() != null);
+	}
+	
+	public void BoardTestWallListNotNull(){
+		Board b = new BoardImplementation(2,2);
+		assert(b.getWallList() != null);
+	}
+
 }
