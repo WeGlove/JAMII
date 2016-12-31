52f9433e463a59b4d20feed516cc4005b2161130
Faeolin9 <johannesihl@yahoo.de>
2016-12-31 12:45:30 +0100

    Test
    Penis

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,12 @@
 package main;
 
-public class Main {
+import org.junit.Test;
 
+public class Main {
+@Test
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//Penis
 	}
 
 
