701a1810a5227d6cf62d96ec96a3868a57c605f3
Faeolin9 <johannesihl@yahoo.de>
2016-12-31 15:43:41 +0100

    Tests for Wall and light added
    Tests whether shit is legit

--- src/testpenis/Tests.java
+++ src/testpenis/Tests.java
@@ -1,4 +1,23 @@
 package testpenis;
-
+import org.junit.Test;
+import board.wall.WallImplementation;
+import board.wall.Wall;
+import board.light.Light;
+import board.light.Lightimplementation;
 public class Tests {//Penis
+	
+	@Test
+	public void WallTestnegint () {
+		int T;
+		Wall w = new WallImplementation ( ~1, ~2, ~3, ~4);
+		T = w.getStartX();
+		assert (T == 1);
+	}
+	@Test
+	public void LightTestnegint () {
+		int T;
+		Light L = new Lightimplementation (~1, ~2);
+		T = L.getX();
+		assert (T == 1);
+	}
 }
